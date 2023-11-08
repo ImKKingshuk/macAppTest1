9a9dfc8bc2ece397d087771a0071f48cc43cbf5b
ImKKingshuk <126203172+ImKKingshuk@users.noreply.github.com>
2023-11-08 07:41:48 +0530

    Initial Commit

---
+++ macAppTest1/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  macAppTest1
+//
+//  Created by Kingshuk Mondal on 08/11/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ macAppTest1/macAppTest1App.swift
@@ -0,0 +1,17 @@
+//
+//  macAppTest1App.swift
+//  macAppTest1
+//
+//  Created by Kingshuk Mondal on 08/11/23.
+//
+
+import SwiftUI
+
+@main
+struct macAppTest1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
